c5b281fc112f13e7a94a3d7cc9c1c85456f131dd
Sjaak Verboom <j.e.verboom@gmail.com>
2024-02-02 11:28:41 +0100

    #New Character Directory

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Yamkola
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
